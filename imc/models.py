99e0a8c33ae516811c5f18899d186815b5bb366f
Fernando <fromero.inmobiliaria@gmail.com>
2022-07-08 18:38:34 +0200

    funcionalidad de imc con base de datos

---
+++ imc/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=20)
+    imc_calculado = models.FloatField()
+
+    def calcular_imc(self,altura, peso):
+        self.imc_calculado = peso / (altura * altura)
+        
